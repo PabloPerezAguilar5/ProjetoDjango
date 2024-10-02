12150af707ad60e7c811604b4afdd9ca386d8a56
pabloperezaguilar5 <pabloperezaguilar@gmail.com>
2024-10-02 18:55:16 -0300

    final commit

--- projetopabloperez/data_loader/admin.py
+++ projetopabloperez/data_loader/admin.py
@@ -5,13 +5,11 @@ from django.urls import path
 from django.template.response import TemplateResponse
 from django.db.models import Count
 
-# Definindo um AdminSite personalizado
 class CustomAdminSite(AdminSite):
     site_header = 'Admin Pablo Perez'
     site_title = 'Admin Custom'
-    index_title = 'Vem vindo ao Admin de Pablo Perez'
+    index_title = 'Bienvenido al Admin de Pablo Perez'
 
-    # URLs personalizadas
     def get_urls(self):
         urls = super().get_urls()
         custom_urls = [
@@ -19,24 +17,26 @@ class CustomAdminSite(AdminSite):
         ]
         return custom_urls + urls
 
-    # visualização do painel
     def dashboard_view(self, request):
+        # Obtener todos os produtos
+        produtos = Produto.objects.all()
+        
+        # Obtenha contagem por categoria
         productos_por_categoria = Produto.objects.values('categoria').annotate(total=Count('id'))
+        
         context = dict(
             self.each_context(request),
+            produtos=produtos,  # Adicione todos os produtos ao contexto
             productos_por_categoria=productos_por_categoria,
         )
         return TemplateResponse(request, "admin/dashboard.html", context)
 
-# Inicialize o AdminSite personalizado
 custom_admin_site = CustomAdminSite(name='custom_admin')
 
-# Registre o modelo do Produto no Admin personalizado
 class ProdutoAdmin(admin.ModelAdmin):
     list_display = ('id', 'nome', 'categoria', 'preco', 'stock')  
     search_fields = ('nome', 'categoria')
     list_filter = ('categoria', 'stock',)  
     ordering = ('-preco',)
 
-# Cadastre o modelo e o administrador
 custom_admin_site.register(Produto, ProdutoAdmin)
